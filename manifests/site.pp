6de85545b9d21c935025ffd232fe0236497eaa20
carlosdgerez <carlosgerez@byui.edu>
2024-10-15 08:16:37 +0200

    creating modules manual and with pdk

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,2 @@
-class { 'apache':
-  default_vhost => false,
-}
-
-apache::vhost { 'vhost.example.com':
-  port    => 80,
-  docroot => '/var/www/vhost',
-}
-
-file { '/var/www/vhost/index.html':
-      content => 'Hello, world',
-
-}
+include profile::base
+include profile::website
